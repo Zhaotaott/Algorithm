7e4f4452b86ad8f879f495b6c9054fea5f0e6f67
Zhaotaott <149968483+Zhaotaott@users.noreply.github.com>
2023-11-06 18:49:46 +0800

    Create input&out.cpp

---
+++ Language/C++/input&out.cpp
@@ -0,0 +1,228 @@
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int a, b;
+//     cin >> a >> b;
+//     cout << a + b;
+//     return 0;
+// }
+
+// #include<iostream>
+
+// using namespace std;
+
+
+// int main()
+// {
+//     int a, b, c, d;
+//     cin >> a >> b >> c >> d;
+//     cout << "DIFERENCA = " << a * b - c * d;
+//     return 0;
+// }
+
+
+// #include<iostream>
+
+// using namespace std;
+
+// const float pi = 3.14159;
+
+// int main()
+// {
+//     int r;
+//     cin >> r;
+//     cout << pi * r * r;
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<cstdio>
+// using namespace std;
+
+// int main()
+// {
+//     float a, b;
+//     cin >> a >> b;
+//     printf("MEDIA = %.5lf", (a * 3.5 + 7.5 * b) / 11);
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int num, hour;
+//     float salary;
+//     cin >> num >> hour >> salary;
+//     printf("NUMBER = %d\nSALARY = U$ %.2lf", num, hour * salary);
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int sum, l;
+//     cin >> sum >> l;
+//     printf("%.3lf km/l", sum / l);
+//     return 0;
+// }
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+
+// int mian()
+// {
+//     float x[2], y[2];
+//     cin >> x[0] >> y[0];
+//     cin >> x[1] >> y[1];
+//     float distance = sqrt(pow(x[0] - x[1], 2) + pow(y[0] - y[1], 2))
+//     printf ("%.4lf", distance);
+//     return 0;
+// }
+
+// #include<iostream>
+
+// using namespace std;
+
+// int face[] = {100, 50, 20, 10, 5, 2, 1};
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     cout << n << endl;
+//     for (int i = 0; i < 7; i++)
+//     {
+//         printf("%d nota(s) de R$ %d,00", n / face[i], face[i]);
+//         n = n - n / face[i] * face[i];
+//     }
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     printf("%d:%d:%d", n / 3600, n % 3600 / 60, n % 3600 % 60);
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int a, b;
+//     cin >> a >> b;
+//     cout << "PROD = " << a * b;
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int num1, num2, n, m;
+//     float salary1, salary2;
+//     cin >> n >> num1 >> salary1;
+//     cin >> m >> num2 >> salary2;
+//     printf("VALOR A PAGAR: R$ %.2lf", num1 * salary1 + num2 * salary2);
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// const float pi = 3.14159;
+
+// int main()
+// {
+//     int r;
+//     cin >> r;
+//     printf("%.3lf", 4.0 / 3 * pi * r * r * r);    
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// const float pi = 3.14159;
+
+// int main()
+// {
+//     float a, b, c;
+//     cin >> a >> b >> c;
+//     cout << a * c / 2 << endl;
+//     cout << c * c * pi << endl;
+//     cout << (a + b) * c / 2 << endl;
+//     cout << b * b << endl;
+//     cout << a * b << endl;
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     float a, b, c;
+//     cin >> a >> b >> c;
+//     printf("%.1lf", (a * 2 + b * 3 + c * 5) / 10);
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     string name;
+//     double salary, sum;
+//     cin >> name >> salary >> sum;
+//     cout << salary + sum * 0.15; 
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int a, b, c;
+//     int max = (a > b ? a : b) > c ? (a > b ? a : b) : c;
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     float distance = (90 - 60) * 1.0 / 60;
+//     float l;
+//     cin >> l;
+//     cout << l / distance;
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int v, t;
+//     cin >> v >> t;
+//     printf("%.3lf", v * t * 1.0 / 12);
+//     return 0;
+// }
