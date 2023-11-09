91bbf32c5601b6bb7c669c0a0dc76b7666430be7
Zhaotaott <149968483+Zhaotaott@users.noreply.github.com>
2023-11-09 23:56:28 +0800

    Update high precision.cpp

--- Basic_Algorithm/high precision/high precision.cpp
+++ Basic_Algorithm/high precision/high precision.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
+// C = A + B, A >= 0, B >= 0
 vector<int> add(vector<int> &A, vector<int> &B)
 {
     if (A.size() < B.size()) return add(B, A);
@@ -21,23 +17,19 @@ vector<int> add(vector<int> &A, vector<int> &B)
     return C;
 }
 
-int main()
+// C = A - B, 满足A >= B, A >= 0, B >= 0
+vector<int> sub(vector<int> &A, vector<int> &B)
 {
-    string a, b;
-    vector<int> A, B;
-    cin >> a >> b;
-    for (int i = a.size() - 1; i >= 0; i -- ) A.push_back(a[i] - '0');
-    for (int i = b.size() - 1; i >= 0; i -- ) B.push_back(b[i] - '0');
+    vector<int> C;
+    for (int i = 0, t = 0; i < A.size(); i ++ )
+    {
+        t = A[i] - t;
+        if (i < B.size()) t -= B[i];
+        C.push_back((t + 10) % 10);
+        if (t < 0) t = 1;
+        else t = 0;
+    }
 
-    auto C = add(A, B);
-
-    for (int i = C.size() - 1; i >= 0; i -- ) cout << C[i];
-    cout << endl;
-
-    return 0;
+    while (C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
 }
-
-作者：yxc
-链接：https://www.acwing.com/activity/content/code/content/39792/
-来源：AcWing
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
